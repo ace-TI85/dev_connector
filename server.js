6f84214f97d8fc64d320f6c24a9abc095bde3aa1
Jonathan Tosio <jtosio@myune.edu.au>
2021-09-20 20:36:43 +1000

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('API running!')
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});
